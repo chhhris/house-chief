012e1daa856e3ff984ca8c598310b84feb8a673b
chhhris <mr.lake@gmail.com>
2015-08-03 15:37:51 -0700

    Nest Items resource under Property

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+  before_action :set_property
   before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   # GET /items
@@ -14,7 +15,7 @@ class ItemsController < ApplicationController
 
   # GET /items/new
   def new
-    @item = Item.new
+    @item = @property.items.new
   end
 
   # GET /items/1/edit
@@ -24,12 +25,12 @@ class ItemsController < ApplicationController
   # POST /items
   # POST /items.json
   def create
-    @item = Item.new(item_params)
+    @item = @property.items.new(item_params)
 
     respond_to do |format|
       if @item.save
-        format.html { redirect_to @item, notice: 'Item was successfully created.' }
-        format.json { render :show, status: :created, location: @item }
+        format.html { redirect_to @property, notice: 'Item was successfully created.' }
+        format.json { render :show, status: :created, location: @property }
       else
         format.html { render :new }
         format.json { render json: @item.errors, status: :unprocessable_entity }
@@ -42,8 +43,8 @@ class ItemsController < ApplicationController
   def update
     respond_to do |format|
       if @item.update(item_params)
-        format.html { redirect_to @item, notice: 'Item was successfully updated.' }
-        format.json { render :show, status: :ok, location: @item }
+        format.html { redirect_to @property, notice: 'Item was successfully updated.' }
+        format.json { render :show, status: :ok, location: @property }
       else
         format.html { render :edit }
         format.json { render json: @item.errors, status: :unprocessable_entity }
@@ -56,15 +57,18 @@ class ItemsController < ApplicationController
   def destroy
     @item.destroy
     respond_to do |format|
-      format.html { redirect_to items_url, notice: 'Item was successfully destroyed.' }
+      format.html { redirect_to property_items_url(@property), notice: 'Item was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
+    def set_property
+      @property = Property.find(params[:property_id])
+    end
     # Use callbacks to share common setup or constraints between actions.
     def set_item
-      @item = Item.find(params[:id])
+      @item = @property.items.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,9 @@ Rails.application.routes.draw do
 
   resources :users
 
-  resources :items
-
-  resources :properties
+  resources :properties do
+    resources :items
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
