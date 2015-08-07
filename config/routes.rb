07c4a58e76117fe6af411632488a2bb34e7000ee
chhhris <mr.lake@gmail.com>
2015-08-07 13:36:52 -0700

    Clean up users routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout', :sign_up => 'signup'}
 
-  resources :users
+  resources :users, except: :index
 
   resources :properties do
     resources :items
