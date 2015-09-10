cc3fd02f49a4cc791698d0d5ca3e5c0d2f7122a7
Chris Lake <chris.lake@ellucian.com>
2015-09-10 08:32:24 -0700

    permit name on devise param sanitizer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,22 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
   def default_url_options
     { host: Rails.env == 'development' ? 'localhost:3000' : 'housechief.com' }
   end
 
   def mailer_set_url_options
     ActionMailer::Base.default_url_options[:host] = 'localhost:3000'
+  end
+
+
+
+protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password) }
   end
 
 end
