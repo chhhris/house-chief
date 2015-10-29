334b95498521a4b7a383e673fb6c76320bf04e4b
Chris Lake <chris.lake@ellucian.com>
2015-10-28 22:08:10 -0700

    display attachments on item show page

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,6 @@ class Item < ActiveRecord::Base
   belongs_to :property
   has_many :item_users
   has_many :users, through: :item_users
-  # has_many :attachments
 
   mount_uploaders :attachments, AttachmentUploader
 
